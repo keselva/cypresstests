8a5d02bda736f793559ca0c486bc5c8ff2173136
Keerthi <keerthiselvaraj@qantas.com.au>
2020-05-09 21:20:39 +1000

    percy token added

--- cypress/integration/PasswordGeneratorTests.js
+++ cypress/integration/PasswordGeneratorTests.js
@@ -6,18 +6,18 @@ describe('Password generator tests', () => {
     beforeEach(() => { 
         cy.visit('/')
         cy.get(passwordPage.pageHeader).should('have.text', 'Experiment')
-        cy.percySnapshot()
+        cy.percySnapshot('Home page')
     })
 
     it('Verify password is generated for english language with special characters true', () => {
         cy.submitrequest('english','true')
         cy.get(passwordPage.password).should('be.visible').should('not.empty') 
-        cy.percySnapshot()   
+        cy.percySnapshot('After generating password with special characters true')   
     })
 
     it('Verify password is generated for english language with special characters false', () => {
         cy.submitrequest('english','false')
         cy.get(passwordPage.password).should('be.visible').should('not.empty')
-        cy.percySnapshot()    
+        cy.percySnapshot('After generating password with special characters false')    
     })
 })
